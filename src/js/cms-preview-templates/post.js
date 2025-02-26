fdddfd6bb7ea3962baf91cb48efda6ef3d4ccaf9
PeteCadman <77100411+PeteCadman@users.noreply.github.com>
2025-02-26 17:02:36 +0000

    again

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -5,6 +5,7 @@ export default class PostPreview extends React.Component {
   render() {
     const {entry, widgetFor, getAsset} = this.props;
     const image = getAsset(entry.getIn(["data", "image"]));
+    const price = getAsset(entry.getIn(["data", "price"]));
 
     return <div className="mw6 center ph3 pv4">
       <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
@@ -14,7 +15,7 @@ export default class PostPreview extends React.Component {
       </div>
       <div className="cms mw6">
         <p>{ entry.getIn(["data", "description"]) }</p>
-        { price &&<p>{ entry.getIn(["data", "price"]) }</p>}
+        { price && <p>{ entry.getIn(["data", "price"]) }</p>}
         { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
         { widgetFor("body") }
       </div>
